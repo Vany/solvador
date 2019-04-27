8178d90f69f9c52ed411fb8bd7fe9d065e89a6db
Arsenii Pechenkin <arspechen@gmail.com>
2019-04-27 18:25:17 +0300

    add structure for notary list page

--- src/app/notaries-list/notaries-list.page.ts
+++ src/app/notaries-list/notaries-list.page.ts
@@ -7,7 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotariesListPage implements OnInit {
 
-  constructor() { }
+  private selectedItem: any;
+
+  public items: Array<{ title: string; metro: string; address: string; phone: string }> = [
+    {
+      title: 'Абдулина Назия Абдульхаковна',
+      metro: 'Сухаревская',
+      address: 'г. Москва, Грохольский пер., д.30, стр.1',
+      phone: '+7 (495) 933-31-24'
+    },
+    {
+      title: 'Авдеева Ирина Владимировна',
+      metro: 'Кузьминки',
+      address: 'г. Москва, Волгоградский просп., д.70',
+      phone: '+7 (495) 172-17-29'
+    },
+    {
+      title: 'Аверина Елена Леонтьевна',
+      metro: 'Бауманская',
+      address: 'г. Москва, Новорязанская ул., д.31/7, кв.18',
+      phone: '+7 (495) 267-51-18'
+    },
+    {
+      title: 'Агаева Аида Балаевна',
+      metro: 'Савеловская',
+      address: 'г. Москва, Башиловская ул., д.1, стр.2',
+      phone: '+7 (495) 213-14-55'
+    },
+    {
+      title: 'Агамиров Натиг Исмаилович',
+      metro: 'Арбатская',
+      address: 'г. Москва, Знаменка ул., д.9/12, оф.8',
+      phone: '+7 (495) 291-44-70'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
     console.log("notaries list init");
