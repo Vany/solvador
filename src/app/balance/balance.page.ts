b7f8499504e4863cfd1fd7dfe289153085a3b7bc
Arsenii Pechenkin <arspechen@gmail.com>
2019-04-27 20:51:03 +0300

    add structure of balance and history page

--- src/app/balance/balance.page.ts
+++ src/app/balance/balance.page.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BalancePage implements OnInit {
 
+  public balances: Array<{ assetName: string; amount: string; }> = [
+    {
+      assetName: 'Ethereum',
+      amount: '100'
+    },
+    {
+      assetName: 'KNC',
+      amount: '1002'
+    }
+  ];
+
+  public txs: Array<{ txHash: string; to: string, block: string, amount: string}> = [
+    {
+      txHash: '0x6c8a34dbdd31da5e4d5a59b3a94d1665964a452e167984ca41f6db9fdc87099e',
+      to: '0x240943d75dd94a28ff7a63f0b4ea71892af87970',
+      block: '7650868',
+      amount: '0.052901833'
+    },
+    {
+      txHash: '0x6c8a34dbdd31da5e4d5a59b3a94d1665964a452e167984ca41f6db9fdc87099e',
+      to: '0x240943d75dd94a28ff7a63f0b4ea71892af87970',
+      block: '7650868',
+      amount: '0.052901833'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
