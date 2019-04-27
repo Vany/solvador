3fb2b028353067e8629717c16872881d627625d6
Arsenii Pechenkin <arspechen@gmail.com>
2019-04-27 21:07:15 +0300

    add structure of balance and history pages

--- src/app/balance/balance.page.ts
+++ src/app/balance/balance.page.ts
@@ -18,20 +18,6 @@ export class BalancePage implements OnInit {
     }
   ];
 
-  public txs: Array<{ txHash: string; to: string, block: string, amount: string}> = [
-    {
-      txHash: '0x6c8a34dbdd31da5e4d5a59b3a94d1665964a452e167984ca41f6db9fdc87099e',
-      to: '0x240943d75dd94a28ff7a63f0b4ea71892af87970',
-      block: '7650868',
-      amount: '0.052901833'
-    },
-    {
-      txHash: '0x6c8a34dbdd31da5e4d5a59b3a94d1665964a452e167984ca41f6db9fdc87099e',
-      to: '0x240943d75dd94a28ff7a63f0b4ea71892af87970',
-      block: '7650868',
-      amount: '0.052901833'
-    }
-  ];
 
   constructor() { }
 
