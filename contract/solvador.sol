3a15f8cb0e28d077fd31cc7303cc9456f9198509
vany <ivan.serezhkin@insolar.io>
2019-04-27 18:04:32 +0300

    contract wip

--- contract/solvador.sol
+++ contract/solvador.sol
@@ -3,20 +3,25 @@ contract SolvadorWallet {
 
     event SomeOneWantsToRecover();
 
-    bytes pubkey;
+    address owner;
     mapping(int => bytes) notary;
 
-
-    constructor(bytes _pk) public {
-        pubkey = _pk;
+    constructor() public {
+        owner = msg.sender;
     }
     
     function() external payable { }
+
+    function transfer(address whoom, uint amount) public returns (bool) {
+        if (msg.sender != owner) return false;
+        return whoom.send(amount);
+    }    
     
     function set_notary(bytes _data) public returns (bool) {
         for (int i = 0; i < 3; i++) {
             if (notary[i].length > 0 ) continue;
             notary[i] = _data;
+            return true;
         }
         return false;
     }
