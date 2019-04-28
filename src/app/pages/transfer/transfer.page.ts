6c76409550c14d38dacbd39ae0c31b5cbb749386
sDrynkina <s.drynkina@bankex.com>
2019-04-28 12:40:03 +0300

    Transfer page structure.

--- src/app/pages/transfer/transfer.page.ts
+++ src/app/pages/transfer/transfer.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-transfer',
-  templateUrl: './transfer.page.html',
-  styleUrls: ['./transfer.page.scss'],
+    selector: 'app-transfer',
+    templateUrl: './transfer.page.html',
+    styleUrls: ['./transfer.page.scss'],
 })
 export class TransferPage implements OnInit {
+    recipient: string;
+    amount: number;
+    assetList: string[] = ['ETH'];
+    asset: string;
 
-  constructor() { }
+    constructor() {
+        this.asset = this.assetList[0];
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    transfer() {
+        console.log(this.amount);
+        console.log(this.recipient);
+        console.log(this.asset);
+    }
+
+    setAsset(asset: string) {
+        this.asset = asset;
+    }
 }
